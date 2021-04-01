22b3d57bddc37f750742459e11aa3e211dcf9383
Zhenya-Ko <56299927+Zhenya-Ko@users.noreply.github.com>
2021-04-01 21:17:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,55 +4,73 @@ import './index.css';
 import data from './db/yan-vermeer';
 
 
-function Square (props) {
+function Square(props) {
   return (
-      <button className="square" style={{backgroundColor:"#000"}}>
-        {props.value}
-      </button>
-    );
-  }
-  
+    <button className="square" style={{ backgroundColor: props.color }} onClick={props.onClick}
+    > 
+    </button>
+  );
+}
+
 class Portrait extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //       squares: Array(data.length).fill(null),
-  //     };
-  //   }
-
-
-    renderSquare() {
-      let squares = [];
-      let bgcolors =[];
-       for (const painting of data) {
-         if(painting.dominant_colors!=="error"){
-       squares = squares.concat(<Square/>);
-       bgcolors = bgcolors.concat ( `rgb ${painting.dominant_colors}`);
-      }
-         }
-      //  const buttons = document.querySelectorAll(".square");
-      //  console.log(buttons);
-              // style={{backgroundColor: `rgb ${painting.dominant_colors}`}}
-      return(squares);          
+  constructor(props){
+    super(props);
+    this.state = {
+      showElement:false,
+      canvas:{},
+      };
     }
 
 
-    render() {
-      return (
-        <div className="frame">
-{this.renderSquare()}
-        </div>
-      );
-      }
+  renderSquare() {
+    return data
+    .filter(painting => painting.dominant_colors !== "error") 
+    .map(painting => <Square  key={painting.FIELD1} onClick={() => this.onClick()} color={`rgb${painting.dominant_colors}`} />)
   }
 
+  findInfo(){
+   this.state.canvas = data.find(painting => painting.FIELD1 === 0)// нужно найти соответствие нажатому элементу через key
+  }
+
+  // paletteColors(){this.state.canvas.palette_сolors.map(e => <div style ={{backgroundColor:`rgb ${e}`}}/>)}
+  //создаю дивы и туда сразу цвет устанавливаю нужный..., но пишет что map is not a function.
+
+  onClick(){
+    this.setState({showElement: !this.state.showElement}) // потом переделать надо будет, чтобы закрывалась если на другую область кликаешь
+    this.findInfo()
+    // this.paletteColors()
+  }
+
+  render() {
+    console.log(this.state.canvas)
+    return (
+      <div className="wrapper">
+      <div className="frame">
+        {this.renderSquare()}
+      </div>
+      {this.state.showElement? 
+        <div className="colorLabel">
+        <h2>Name <span>({this.state.canvas.Date})</span></h2>
+        <img src={this.state.canvas.urls}></img> 
+        <p>Palette Colors</p>
+        <div className="dominantColors ">
+        {/* {this.paletteColors} */}
+        </div>
+        </div>
+        : null
+        }
+      </div>
+    );
+  }
+}
+
 class Paintercard extends React.Component {
-  render(){
-  return (
-    <div className="card">
-<p>Ян Вермеер</p>
-<Portrait/>
-    </div>
+  render() {
+    return (
+      <div className="card">
+        <h1>Ян Вермеер</h1>
+        <Portrait />
+      </div>
     );
   }
 }
