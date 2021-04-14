6c2700bc93281b358b1f96636eeba629ab041440
ZhenyaK <zhenya.koroleva.2011@mail.ru>
2021-04-14 14:43:23 +0300

    Popup +a bit of styling

--- src/index.js
+++ src/index.js
@@ -9,16 +9,57 @@ import insta from "./images/insta.png"
 import fb from "./images/fb.png"
 import vk from "./images/vk.svg"
 
-function MainScreen(){
+class MainScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPopup: false,
+        }
+    }
+
+popupOpen(){this.setState({showPopup: true})}
+
+closePopup(){this.setState({showPopup:false})}
+
+
+render() {
     return (
         <header>  
     <p id="projectname">Digital Portraits</p>
-        <button id="about">?</button>
+        <button id="about" onClick={() => this.popupOpen()}>?</button>
         <div id ="backimg">
             <p>Look at your favourite painters from different perspective</p>
         </div>
+        <CSSTransition in={this.state.showPopup} timeout ={300} classNames="popup" unmountOnExit>
+                    <div className="popupbody">
+                    <div className="close" onClick={() => this.closePopup()}>X</div>
+                    <div id="dopinfo" >*</div>
+                    <h2>About</h2>
+                    <p>These visualizations allow to look at chosen artists from color perspective. </p>
+                    <img alt="Digital portrait"/>
+                    <p>Each digital canvas is constructed from 
+                        squares representing dominant colors of 
+                        different paintings. They are placed in 
+                        chronological order, therefore occasionally
+                        it is possible to spot the difference between
+                        color use within painter’s life.
+                    </p>
+                    <img alt="Screenshot of the page"/>
+                    <p>The data was parsed from WikiArt website. 
+                        Later the dominant and palette colors were 
+                        identified using Phython libraries.
+                        Then the dataset was filtered to get 
+                        rid of sculptures, installations, drawings
+                         and other not relative works.
+                         </p>
+                    <p>This project was completed as a part of a
+                         DH center Seeds Grant in 2020-2021
+                         </p>
+                    </div>
+                    </CSSTransition>
         </header>
         );
+}
 }
 
 class Filters extends React.Component{
@@ -40,12 +81,12 @@ class Filters extends React.Component{
 
 function Square(props) {
 
-const side = Math.sqrt(250*250/data.length).toFixed(3)  
-//Нужно установить автоматическую ширину кнопок, чтобы они заполнили все пространство дива. и не выходили за его пределы. ???
+// const side = Math.sqrt(250*250/data.length).toFixed(3)  
+
     return (
         <button
             className="square"
-            style={{height:`${side}px`,width:`${side}px`, backgroundColor: props.color}}
+            style={{ backgroundColor: props.color}}
             onClick={props.onClick}
         />
     );
@@ -76,15 +117,15 @@ class Portrait extends React.Component {
         this.setState({
             canvas: data[id]
         })
-    this.paletteColors(id)
+    // this.paletteColors(id)
     }
 
-    paletteColors(id){
-        let reg =/\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/g; 
-        let palette = data[id].palette_colors.match(reg);
-    palette.map(e =>
-   <div style ={{backgroundColor:`rgb ${e}`}}/>)
-}
+//     paletteColors(id){
+//         let reg =/\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/g; 
+//         let palette = data[id].palette_colors.match(reg);
+//     palette.map(e =>
+//    <div style ={{backgroundColor:`rgb ${e}`}}/>)
+// }
 
 
   closeModal(){
