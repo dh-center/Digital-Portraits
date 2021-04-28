bd40f64e74bd0d79fa789f791413a410559bbd43
ZhenyaK <zhenya.koroleva.2011@mail.ru>
2021-04-29 01:48:25 +0300

    roman centuries

--- src/Filters.js
+++ src/Filters.js
@@ -4,10 +4,14 @@ import './filters.css';
 function Filters(props) {
 
     const [centValue, setcentValue] = useState(15)
+    const [romcentValue, setromcentValue] = useState("")
+
+    const romanCentury = {15:'XV', 16:'XVI', 17:'XVII', 18:'XVIII', 19:'XIX', 20:'XX'}
 
     const handleChange = (e) => {
         setcentValue(e.target.value);
         props.filtering(parseInt(e.target.value));
+        setromcentValue(romanCentury[e.target.value])
     };
 
     const handleClick = () => {
@@ -22,8 +26,9 @@ function Filters(props) {
             <p>Movement
                 <input id="movement" type="text" placeholder="Impressionism" />
             </p>
-            <p>Century {centValue}
-                <span className="input_container">
+            <p>Century
+                <span className="century_container">
+                    <span className="century_label">{romcentValue}</span>
                     <input id="century" type="range" min={15} max={20} value={centValue} step="1"
                         onChange={(e) => handleChange(e)}
                     />
