708f64816fb127629efba4d89da07f10831da602
ZhenyaK <zhenya.koroleva.2011@mail.ru>
2021-05-03 13:42:46 +0300

    small step

--- src/Filters.js
+++ src/Filters.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './filters.css';
 
-class Filters extends React.Component {
-    render() {
-        return (
-            <div id="filterCont">
-                <h2>Filter painters:</h2>
-                <p>Movement
-                <input id="movement" type="text" placeholder="Impressionism" />
-                </p>
-                <p>Century
-                <input id="century" type="range" min="XVI" max="XX" step="1" />
-                </p>
 
-            </div>
-        );
+function Filters(props) {
+
+    const [movement, setMovementValue] = useState("")
+
+    const handleMovementChange = (e) => {
+        setMovementValue(e.target.value)
+        if (e.target.value === ""){
+            props.resetcards();
+        }
     }
+
+    return (
+        <div id="filterCont">
+            <h2>Filter painters:</h2>
+            <p>Movement
+                <select value={movement} onChange={(e) => handleMovementChange(e)}>
+                    <option value="">All</option>
+                    <option value="Early Renaissance">Early Renaissance</option>
+                    <option value="Northern Renaissance">Northern Renaissance</option>
+                    <option value="High Renaissance">High Renaissance</option>
+                    <option value="Baroque">Baroque</option>
+                    <option value="Rococo">Rococo</option>
+                    <option value="Neoclassicism">Neoclassicism</option>
+                    <option value="Romanticism">Romanticism</option>
+                    <option value="Impressionism">Impressionism</option>
+                    <option value="Realism">Realism</option>
+                    <option value="Post-Impressionism">Post-Impressionism</option>
+                    <option value="Symbolism">Symbolism</option>
+                    <option value="Expressionism">Expressionism</option>
+                    <option value="Abstract Art">Abstract Art</option>
+                    <option value="Analytical Realism">Analytical Realism</option>
+                    <option value="Suprematism">Suprematism</option>
+                    <option value="Primitivism">Primitivism</option>
+                    <option value="Surrealism">Surrealism</option>
+                    <option value="Abstract Expressionism">Abstract Expressionism</option>
+                    <option value="Pop Art">Pop Art</option>
+                </select>
+            </p>
+            <p>Century
+                <input id="century" type="range" min="XVI" max="XX" step="1" />
+            </p>
+
+        </div>
+    );
 }
 
 export default Filters
