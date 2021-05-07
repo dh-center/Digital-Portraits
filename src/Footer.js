08b2a2e09ba34ed52220f6c78876a493bb590f64
ZhenyaK <zhenya.koroleva.2011@mail.ru>
2021-05-07 18:21:09 +0300

    new things

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,7 @@ import './footer.css';
 import insta from "./images/insta.png"
 import fb from "./images/fb.png"
 import vk from "./images/vk.svg"
+import git from "./images/git1.svg"
 
 import logo from "./images/dh_logo.png"
 
@@ -12,8 +13,9 @@ function Footer() {
     return (
         <footer>
             <img id="logo" alt="logo" src={logo} />
-            <p id="contacts">Contact us: dh@itmo.ru</p>
+            <p className="contacts">Contact us: <a href="mailto:dh@itmo.ru"> dh@itmo.ru </a> </p>
             <div id="socialmedia">
+            <a href="https://github.com/dh-center/Digital-Portraits"><img src={git} alt=" Instagram " /></a>
                 <a href="https://www.instagram.com/dh_center/"><img src={insta} alt=" Instagram " /></a>
                 <a href="https://vk.com/dhcenter"><img src={vk} alt="Facebook " /></a>
                 <a href="https://www.facebook.com/dhcenter.itmo/"><img src={fb} alt="VK " /></a>
